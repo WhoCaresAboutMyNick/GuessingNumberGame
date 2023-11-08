6e9214ba40c2e170089f38a9a2533fd899d4de78
Forest <96078954+forestanonymous@users.noreply.github.com>
2023-11-08 14:59:06 +0100

    Update main.py
    added a few features

--- main.py
+++ main.py
@@ -3,33 +3,47 @@ import random as r
 guesses = 0
 number = r.randint(1,10)
 
-while guesses<3:
-	q1=int(input("Guess the number(1,10): "))
+while True:
+    q1=int(input("Guess the number(1,10): "))
+    
+    if q1==number:
+        print(f"\nright, the number was {number}!")
+        number = r.randint(1,10)
+        guesses=0
 
-	if q1==number:
-		print(f"right, the number was {number}!")
-		number = r.randint(1,10)
-		guesses=0
+        rep=str(input("\nDo you want to play again?(y/..): ")).lower()
+        print(" ")
 
-		rep=str(input("Do you want to play again?(y/n): ")).lower()
+        if rep=="y":
+            pass
+        else:
+            break
+            
+    elif q1>10:
+        print("\nYou can choose only numbers ranging from 1 to 10.\n")        
 
-		if rep=="y":
-			pass
-		else:
-			break
+    else:
+        guesses+=1
+        if q1<number and guesses==3:
+            pass
+        elif q1>number and guesses==3:
+            pass
+        elif q1<number:
+            print("\nGuess higher.\n")
+        elif q1>number:
+            print("\nGuess lower.\n")
+          
+            
 
-	else:
-		print("wrong")
-		guesses+=1
-
-	if guesses==3:
-		guesses=0
-		print(f"You lost, the number was {number}!")
-
-		rep=str(input("Do you want to play again?(y/n): ")).lower()
-		number = r.randint(1,10)
-		
-		if rep=="y":
-			pass
-		else:
-			break
+    if guesses==3:
+        guesses=0
+        print(f"\nYou lost, the number was {number}!")
+        
+        rep=str(input("\nDo you want to play again?(y/..): ")).lower()
+        number = r.randint(1,10)
+        
+        if rep=="y":
+            print(" ")
+        else:
+            break
+               
